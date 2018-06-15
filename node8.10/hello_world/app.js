bb377a784c847dffc73c20488efb5ef2b4ad8e57
Jack Warner <jack.warner@parivedasolutions.com>
2018-06-15 14:38:25 -0400

    SAM starters for Node8.10 and Java 8

---
+++ node8.10/hello_world/app.js
@@ -0,0 +1,25 @@
+
+const axios = require('axios')
+const url = 'http://checkip.amazonaws.com/';
+let response;
+
+
+exports.lambda_handler = async (event, context, callback) => {
+    try {
+        const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello boo',
+                goodstuff: 'jack',
+                location: ret.data.trim()
+            })
+        }
+    }
+    catch (err) {
+        console.log(err);
+        callback(err, null);
+    }
+
+    callback(null, response)
+};
